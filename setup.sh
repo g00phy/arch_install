3c1ff153acdd98a4cdd1a7c5190b06f7b822d2f8
g00phy <gopalakrishnar91@gmail.com>
2024-06-02 23:56:29 +0500

    Update setup.sh -8

--- setup.sh
+++ setup.sh
@@ -1,26 +1,15 @@
+
+yes | sudo pacman -S reflector
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+sudo reflector --verbose --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+
 yes | sudo pacman -R blueberry
 
-yes | sudo pacman -S bluez
-yes | sudo pacman -S bluez-utils
-yes | sudo pacman -S blueman
+yes | sudo pacman -S bluez bluez-utils  blueman
 sudo systemctl enable bluetooth.service
 
-cd /run/media/g00phy/EVA-3/apps/
-sudo usermod -aG kvm $USER
-sudo pacman -S gnome-terminal
-
-sudo cp docker/* /usr/bin/
-sudo dockerd &
-sudo docker run hello-world
-
-sudo pacman -U ./docker-desktop-4.30.0-x86_64.pkg.tar.zst
-systemctl --user start docker-desktop
-systemctl --user enable docker-desktop
-cd /etc/systemd/system/
-mkdir docker.service
-
-yes | sudo pacman -Syu vlc keepassxc deluge thunderbird firefox spotify-launcher python-pip sassc unzip file-roller eog emby-server
-systemctl start emby-server
+yes | sudo pacman -Syu vlc keepassxc deluge thunderbird firefox spotify-launcher python-pip sassc unzip file-roller eog libreoffice-fresh gimp thunderbird ntfs-3g
 
 sudo pacman -S --needed base-devel 
 cd /run/media/g00phy/EVA-3/apps/yay
@@ -45,3 +34,5 @@ cd gtk
 source gtk-env/bin/activate
 python3 build.py mocha --dest /usr/share/themes -a blue --tweaks black
 
+yay -Sy timeshift
+yay -S prowlarr
