6a13a2689c89eee43079a7afd0734cdc66a5414b
g00phy <gopalakrishnar91@gmail.com>
2024-06-13 19:18:58 +0530

    Update setup.sh 0

--- setup.sh
+++ setup.sh
@@ -17,15 +17,6 @@ makepkg -si
 yay --version
 yay -S lazydocker
 
-cd /run/media/g00phy/EVA-3/apps/snapd
-makepkg -si
-sudo systemctl enable --now snapd.socket
-sudo systemctl enable --now snapd.apparmor.service
-sudo ln -s /var/lib/snapd/snap /snap
-sudo snap install pycharm-professional --classic
-
-sudo snap install slack
-
 cd /run/media/g00phy/EVA-3/apps/themes
 cp -r NovaOS-nord-Theme/NovaOS-nord/ /usr/share/themes/
 cp -r NovaOS-nord-Icons/NovaOS-nord/ /usr/share/icons/
@@ -36,3 +27,4 @@ python3 build.py mocha --dest /usr/share/themes -a blue --tweaks black
 
 yay -Sy timeshift
 yay -S prowlarr
+yay -S pycharm-professional
