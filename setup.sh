7047d19fe4a62a1839946e2c47175518f9dc61e9
g00phy <gopalakrishnar91@gmail.com>
2024-06-30 21:26:34 +0530

    Update setup.sh 34

--- setup.sh
+++ setup.sh
@@ -7,23 +7,23 @@ git clone https://aur.archlinux.org/paru.git
 cd paru
 makepkg -si
 
-yes | sudo paru -S reflector
+yes | paru -S reflector
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 sudo reflector --verbose --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
-yes | sudo paru -R blueberry
+yes | paru -R blueberry
 
-yes | sudo paru -S bluez bluez-utils  blueman
-sudo systemctl enable bluetooth.service
+yes | paru -S bluez bluez-utils  blueman
+systemctl enable bluetooth.service
 
-yes | sudo paru -Syu vlc deluge deluge-gtk
-yes | sudo paru -S spotify-launcher python-pip sassc unzip file-roller eog apple-fonts
-yes | sudo paru -S libreoffice-fresh gimp 
-sudo paru -S thunderbird 
-sudo pacman -S nvidia-settings 
-sudo paru -S kate
-sudo paru -S emby-server
-sudo systemctl enable emby-server.service
+yes | paru -Syu vlc deluge deluge-gtk
+yes | paru -S spotify-launcher python-pip sassc unzip file-roller eog apple-fonts
+yes | paru -S libreoffice-fresh gimp 
+paru -S thunderbird 
+pacman -S nvidia-settings 
+paru -S kate
+paru -S emby-server
+systemctl enable emby-server.service
 
 curl -fsSL https://get.docker.com/rootless | sh
 echo 'export PATH=/home/g00phy/bin:$PATH' >> ~/.bashrc
