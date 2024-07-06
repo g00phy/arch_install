b9e2e5d61124e86c5246f9bcd78af0d0038d13e0
g00phy <gopalakrishnar91@gmail.com>
2024-07-06 20:40:12 +0000

    Update setup.sh 38

--- setup.sh
+++ setup.sh
@@ -24,8 +24,8 @@ paru -S emby-server
 systemctl enable emby-server.service
 
 curl -fsSL https://get.docker.com/rootless | sh
-export PATH=/home/g00phy/bin:$PATH
-export DOCKER_HOST=unix:///run/user/1000/docker.sock
+echo "export PATH=/home/g00phy/bin:$PATH"
+echo "export DOCKER_HOST=unix:///run/user/1000/docker.sock"
 systemctl --user start docker
 systemctl --user enable docker
 sudo loginctl enable-linger $(whoami)
