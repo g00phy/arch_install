b90bd83de4deb85720af73765d1521bf5029f706
g00phy <gopalakrishnar91@gmail.com>
2024-07-18 16:46:26 +0530

    Update archinstall.sh 5

--- archinstall.sh
+++ archinstall.sh
@@ -20,7 +20,7 @@ echo -ne "
       source $CONFIGS_DIR/setup.conf
     ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
     ( $HOME/arch_install/scripts/1-setup.sh )|& tee 1-setup.log
-    ( /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/arch_install/scripts/2-user.sh )|& tee 2-user.log
+    ( /home/$USERNAME/arch_install/scripts/2-user.sh )|& tee 2-user.log
     ( $HOME/arch_install/scripts/3-post-setup.sh )|& tee 3-post-setup.log
     ( $HOME/arch_install/scripts/4-rootless-docker.sh )|& tee 4-rootless-docker.log
     cp -v *.log /mnt/home/$USERNAME
