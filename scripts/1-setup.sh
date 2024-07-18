c2db2cd364a2f7a19308d387accccea9519556d5
g00phy <gopalakrishnar91@gmail.com>
2024-07-18 19:55:46 +0530

    Update 1-setup.sh 9

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -8,8 +8,8 @@ echo -ne "
                     Setting up mirrors for optimal download
 -------------------------------------------------------------------------
 "
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-reflector --verbose --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+sudo reflector --verbose --latest 10 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 echo -ne "
 -------------------------------------------------------------------------
                     SYSTEM READY FOR 2-user.sh
