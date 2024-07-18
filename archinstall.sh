437dd75c73141deeccb043db102b133f60e69590
g00phy <gopalakrishnar91@gmail.com>
2024-07-18 19:08:08 +0530

    Update archinstall.sh 6

--- archinstall.sh
+++ archinstall.sh
@@ -19,12 +19,14 @@ echo -ne "
     ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
       source $CONFIGS_DIR/setup.conf
     ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
-    ( $HOME/arch_install/scripts/1-setup.sh )|& tee 1-setup.log
-    ( $HOME/arch_install/scripts/2-user.sh )|& tee 2-user.log
-    ( $HOME/arch_install/scripts/3-post-setup.sh )|& tee 3-post-setup.log
-    ( $HOME/arch_install/scripts/4-rootless-docker.sh )|& tee 4-rootless-docker.log
-    cp -v *.log /home/$USERNAME
-
+    ( arch-chroot /mnt $HOME/arch_install/scripts/1-setup.sh )|& tee 1-setup.log
+    if [[ ! $DESKTOP_ENV == server ]]; then
+      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/arch_install/scripts/2-user.sh )|& tee 2-user.log
+    fi
+    ( arch-chroot /mnt $HOME/arch_install/scripts/3-post-setup.sh )|& tee 3-post-setup.log
+    ( arch-chroot /mnt $HOME/arch_install/scripts/4-rootless-docker.sh  )|& tee 4-rootless-docker.log
+    cp -v *.log /mnt/home/$USERNAME
+   
 echo -ne "
 -------------------------------------------------------------------------
                 Done - Please Reboot
