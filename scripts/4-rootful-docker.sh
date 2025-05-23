b4ef2ec94b44a173d5372467e89f339ef16b45c2
g00phy <gopalakrishnar91@gmail.com>
2025-05-24 01:35:03 +0530

    rootful docker changes

--- scripts/4-rootful-docker.sh
+++ scripts/4-rootful-docker.sh
@@ -14,24 +14,18 @@ source "$CONFIGS_DIR"/setup.conf
 
 echo -ne "rootless docker"
 # Install fuse-overlayfs
-sudo pacman -S fuse-overlayfs
-
-# Add kernel.unprivileged_userns_clone=1 to /etc/sysctl.conf
-echo "kernel.unprivileged_userns_clone=1" | sudo tee -a /etc/sysctl.conf
+sudo pacman -S --noconfirm --needed docker nvidia-container-toolkit
 
 # Apply the sysctl changes
-sudo sysctl --system
 sudo systemctl start docker.service
 sudo systemctl enable docker.service
 sudo usermod -aG docker $USER
 echo "docker enabled"
 
 echo -ne "nvidia-container-toolkit "
-sudo pacman -S --noconfirm --needed nvidia-container-toolkit
 # shellcheck disable=SC2086
-sudo nvidia-ctk runtime configure --runtime=docker --config=$HOME/.config/docker/daemon.json
-systemctl --user restart docker
-sudo nvidia-ctk config --set nvidia-container-cli.no-cgroups --in-place
+sudo nvidia-ctk runtime configure --runtime=docker
+sudo systemctl restart docker
 echo "  nvidia docker enabled"
 
 echo -ne "rootless docker buildx"
